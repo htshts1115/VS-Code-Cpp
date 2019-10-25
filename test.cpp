7e427eaba2cafdb43d2981a6362784bd64fc5450
T.Huang <htshts1115@gmail.com>
2019-10-25 12:06:39 +0800

    practice iterator code

--- test.cpp
+++ test.cpp
@@ -1,48 +1,62 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
 
-using namespace std;
-//基类
 
-class D
-{
-public:
-    D(){cout<<"D()"<<endl;}
-    ~D(){cout<<"~D()"<<endl;}
-protected:
-    int d;
-};
+// //基类
+// class D
+// {
+// public:
+//     D(){cout<<"D()"<<endl;}
+//     ~D(){cout<<"~D()"<<endl;}
+// protected:
+//     int d;
+// };
 
-class B:virtual public D
-{
-public:
-    B(){cout<<"B()"<<endl;}
-    ~B(){cout<<"~B()"<<endl;}
-protected:
-    int b;
-};
+// class B:virtual public D
+// {
+// public:
+//     B(){cout<<"B()"<<endl;}
+//     ~B(){cout<<"~B()"<<endl;}
+// protected:
+//     int b;
+// };
 
-class A:virtual public D
-{
-public:
-    A(){cout<<"A()"<<endl;}
-    ~A(){cout<<"~A()"<<endl;}
-protected:
-    int a;
-};
+// class A:virtual public D
+// {
+// public:
+//     A(){cout<<"A()"<<endl;}
+//     ~A(){cout<<"~A()"<<endl;}
+// protected:
+//     int a;
+// };
 
-class C:public B, public A
-{
-public:
-    C(){cout<<"C()"<<endl;}
-    ~C(){cout<<"~C()"<<endl;}
-protected:
-    int c;
-};
+// class C:public B, public A
+// {
+// public:
+//     C(){cout<<"C()"<<endl;}
+//     ~C(){cout<<"~C()"<<endl;}
+// protected:
+//     int c;
+// };
 
 int main()
 {
-    cout << "Hello World!" << endl;
-    C c;   //D, B, A ,C
-    cout<<sizeof(c)<<endl;
+    // C c;   //D, B, A ,C
+    // std::cout<<sizeof(c)<<std::endl;
+
+    std::vector<std::string> names;
+    names.push_back("James");
+    names.push_back("Tom");
+    names.push_back("Kelly");
+
+    std::sort(names.begin(), names.end());
+
+    std::vector<std::string>::iterator iter= names.begin();
+    while (iter != names.end()) {
+        std::cout << *iter << std::endl;
+        iter++;
+    }
+
     return 0;
 }
